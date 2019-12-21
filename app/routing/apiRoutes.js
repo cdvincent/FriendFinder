d93430ec39564ea0b55cd4ff8aa6fc78c462347a
Chris Vincent <cdvincent89@gmail.com>
2019-12-21 15:07:12 -0500

    added modal and friend matching logic

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,12 +1,39 @@
-var friendsData = require("../data/friendsData");
+var friends = require("../data/friends");
 
 module.exports = function(app) {
 
   app.get("/api/friends", function(req, res) {
-      res.json(friendsData);
-    });
+      res.json(friends);
+  });
 
   app.post("/api/friends", function(req, res) {
-    friendsData.push(req.body);
+
+    let newFriend = req.body;
+    let newFriendTotal = 0;
+
+    for (let i = 0; i < newFriend.scores.length; i++) {
+      newFriend.scores[i] = parseInt(newFriend.scores[i]);
+      newFriendTotal += newFriend.scores[i];
+    }
+    
+
+    let bestFriend = 49;
+    let index = 0;
+    let friendTotal = 0;
+    
+    for (let j = 0; j < friends.length; j++) {
+      let friendDiff = 0;
+      for (let k = 0; k < friends[j].scores.length; k++) {
+        friendTotal += friends[j].scores[k];
+        friendDiff = Math.abs(newFriendTotal - friendTotal);
+      }
+      if (friendDiff <= bestFriend) {
+        bestFriend = friendDiff;
+        index = j;
+      }
+      friendTotal = 0;
+    }
+    friends.push(newFriend);
+    res.json(friends[index]);
   });
 }
